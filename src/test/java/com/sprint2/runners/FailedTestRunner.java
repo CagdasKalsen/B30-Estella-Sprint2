8c827b9500b0005d65997c621c7e65ce23e81a96
CagdasKalsen <cagdaskalsen@hotmail.com>
2023-10-29 17:42:05 -0400

    Project Set Up

---
+++ src/test/java/com/sprint2/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.sprint2.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/sprint2/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
